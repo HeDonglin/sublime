cc0a8a942d6e1a7e76a2f82857f5b88251bbff5f
w3c0929 <w3c0929@163.com>
2017-08-03 12:20:21 +0800

    sublime3126

---
+++ Packages/Browser Sync/browser_sync_launch.js
@@ -0,0 +1,18 @@
+var bs = require('browser-sync').create('ST3');
+
+args = process.argv.slice(2);
+
+bs.init({
+	server:args[0],
+	files:args[1].split(','),
+	index:args[2],
+	startPath:args[2],
+	logLevel:"silent",
+    browser:"chrome",
+    notify: false, //浏览器右上角小提示
+    port:9090
+    // browser: ["chrome"], //可以配置多个浏览器
+    // files: args[1].split(','), //监听文件必备
+    // proxy: 'localhost:3000', //连接到phpstudy服务器端口(配合php调试)
+    // injectChanges: false//如果为true即是注入css,并不会刷新页面,对于@import方式不友好,所以改为false来刷新页面;
+});
